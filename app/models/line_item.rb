3185bd828a88f16f126b893c569c2c19d986421e
VirtualClinics <virtualclinics@ubuntuserver>
2017-01-13 10:36:10 -0500

    update on line_item.rb

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,10 +3,6 @@ class LineItem < ActiveRecord::Base
    belongs_to :product
    belongs_to :cart
    def total_price
-    if product.price
        product.price = product.price * quantity
-     else
-        0.to_d
-     end
    end
 end
