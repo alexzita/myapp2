0f7b28dd16eda3d2e63935e231d6b3e8b4baa62b
VirtualClinics <virtualclinics@ubuntuserver>
2017-01-13 08:07:24 -0500

    correct bigdecimal error

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,10 @@ class LineItem < ActiveRecord::Base
    belongs_to :product
    belongs_to :cart
    def total_price
-       product.price * quantity
+    if product.price
+       product.price = product.price * quantity
+     else
+        0.to_d
+     end
    end
 end
